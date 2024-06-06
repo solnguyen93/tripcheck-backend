9b334e9e04570c854b634ad8325ade5decb026f8
Sol Nguyen <solnguyen93@gmail.com>
2024-06-05 21:57:19 -0700

    db

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env' });
 
-console.log('pguser', process.env.PGUSER);
-
 const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
